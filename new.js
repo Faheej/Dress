b8c4b8262883c0dff93d47caaf8685e29c31855b
Faheej <fahee02j@gmail.com>
2025-05-20 12:33:14 +0530

    unwanted file deleted

--- new.js
+++
@@ -1,5 +0,0 @@
-// alert(err.response?.data?.message || "Internal Server Error")
-showToast(false, err.response?.data?.message || "Internal Server Error")
-
-// alert(response.data.message)
-showToast(true, response.data.message)
